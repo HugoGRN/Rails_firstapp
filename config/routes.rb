d385e2ae75d97415789d256e2be358789788a427
Hugo <h.granier@me.com>
2022-12-08 15:12:08 +0100

    CRUD presque ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :articles
+
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'blog', to: 'pages#blog'
