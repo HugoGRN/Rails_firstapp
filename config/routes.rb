d855b9305430104df60363e34a0a1102378e1704
Hugo <h.granier@me.com>
2022-12-19 21:46:07 +0100

    edit action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :articles
-
+  
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
+
   get 'blog', to: 'articles#index'
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   
   root "pages#home"
 
