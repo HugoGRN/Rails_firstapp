15b27ed1b47146a8ef9b57603a4ee742b549e711
Hugo <h.granier@me.com>
2022-12-20 17:58:36 +0100

    authentification method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
    end 
 
    def create
-       render 'new'
+       user = User.find_by(email: params[:session][:email].downcase)
+       if user && user.authenticate(params[:session][:password])
+            flash[:succes] = "Le retour de l'élu !"
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:danger] = "C'est pas carré morray"
+            render 'new'
+       end
    end
 
    def destroy
-       
+       session[:user_id] = nil
+       flash[:succes] = "C'est ça casse toi"
+       redirect_to home_path
    end
 
 end
